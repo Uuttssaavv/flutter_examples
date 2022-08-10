bfad66cbd250438af7a0c6b44c1b636af92e48b3
Utsav Ghimire <ghimireutsav11@gmail.com>
2022-08-10 17:06:43 +0545

    extending equatable class to compare equality

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,21 @@
+import 'package:equatable/equatable.dart';
+
 void main() {
-  final person1 = Person('Utsav', 23, 987654321);
-  final person2 = Person('Ghimire', 23, 987654321);
-  final person3 = Person('Utsav', 23, 987654321);
+  const person1 = Person('Utsav', 23, 987654321);
+  const person2 = Person('Ghimire', 23, 987654321);
+  const person3 = Person('Utsav', 23, 987654321);
   print(person1 == person2);
   print(person1 == person3);
   print(person2 == person3);
 }
 
-class Person {
+class Person extends Equatable {
   final String name;
   final int age;
   final dynamic phone;
 
-  Person(this.name, this.age, this.phone);
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is Person &&
-          runtimeType == other.runtimeType &&
-          name == other.name &&
-          age == other.age &&
-          phone == other.phone;
+  const Person(this.name, this.age, this.phone);
 
   @override
-  int get hashCode => name.hashCode ^ age.hashCode ^ phone.hashCode;
+  List<Object?> get props => [name, age, phone];
 }
