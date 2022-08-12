487dac869339ac78be27112c32d3724da1a598d4
Utsav Ghimire <ghimireutsav11@gmail.com>
2022-08-12 23:07:09 +0545

    homepage with products

--- lib/src/main/homepage.dart
+++ lib/src/main/homepage.dart
@@ -1,16 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project/src/providers/product_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final homeProvider = StateProvider((ref) => 1);
 
-class Homepage extends StatelessWidget {
+class Homepage extends ConsumerWidget {
   const Homepage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final productState = ref.watch(productNotifierProvider);
+    final products = productState.products;
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Homepage'),
+      ),
       body: SafeArea(
-        child: Container(),
+        child: productState.isLoading
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : productState.errorMessage != ''
+                ? Center(
+                    child: Text('The error is ${productState.errorMessage}'),
+                  )
+                : ListView.separated(
+                    separatorBuilder: (_, __) => const Divider(),
+                    itemCount: products.length,
+                    itemBuilder: (_, index) => ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage:
+                            NetworkImage(products[index].thumbnail),
+                      ),
+                      title: Text(
+                        products[index].title,
+                      ),
+                      subtitle: Text(products[index].description),
+                    ),
+                  ),
       ),
     );
   }
