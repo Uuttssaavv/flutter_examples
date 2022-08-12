24326ac33c086193aa722e02a5a0787568f22426
Utsav Ghimire <ghimireutsav11@gmail.com>
2022-08-12 23:06:28 +0545

    created service class for making api requests

---
+++ lib/src/services/product_service.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_project/shared/api.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final productProvider = Provider(ProductService.new);
+
+class ProductService {
+  //
+  final ProviderRef ref;
+  ApiService get apiService => ref.read(apiServiceProvider);
+
+  ProductService(this.ref);
+  Future<dynamic> fetchProducts() async {
+    var response = await apiService.dio.get('/products');
+    return response.data;
+  }
+}
